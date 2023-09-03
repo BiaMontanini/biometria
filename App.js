e5eccfffba680c8063b9850a4ee8e165e6feb837
Beatriz R. Montanini <montanini249@gmail.com>
2023-09-03 00:07:26 -0300

    tipos verificado

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import { styles } from './styles';
 import {View, Text} from "react-native"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
-  const [isAunthenticate ] = useState();
+  const [isAunthenticated, setIsAuthenticated ] = useState(false);
+
+  async function verifyAvaiableAuthentication(){
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible)
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types.map(type=> LocalAuthentication.AuthenticationType[type]))
+  }
+
+  useEffect(() =>{
+    verifyAvaiableAuthentication();}, []);
 
   return (
     <View style={styles.container}>
-      <Text>Usuario conectado:</Text>
-      <StatusBar style="auto" />
+      <Text>Usuario conectado: {isAunthenticated ? 'Sim': 'Não'}</Text>
+
     </View>
   );
 }
