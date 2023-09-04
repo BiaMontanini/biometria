e09b0a80510a3b9ade148476d537adcbe78d19b9
Beatriz R. Montanini <montanini249@gmail.com>
2023-09-03 21:45:58 -0300

    biometria sendo reconhecida

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { styles } from './styles';
-import {View, Text} from "react-native"
+import {View, Text, Button, Alert} from "react-native"
 import { useEffect, useState } from 'react';
 import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
   const [isAunthenticated, setIsAuthenticated ] = useState(false);
+
+  async function handleAuthentication(){
+
+const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+console.log(isBiometricEnrolled)
+if(!isBiometricEnrolled){
+  return Alert.alert('Biometria não encontrada.', 'Por favor, cadastre sua digital no dispositivo!');
+}
+
+const auth = await LocalAuthentication.authenticateAsync({
+  promptMessage: 'Login com Biometria',
+  fallbackLabel: 'Biometria não reconhecida'
+});
+
+setIsAuthenticated(auth.success)
+
+  }
 
   async function verifyAvaiableAuthentication(){
     const compatible = await LocalAuthentication.hasHardwareAsync();
@@ -21,6 +39,10 @@ export default function App() {
     <View style={styles.container}>
       <Text>Usuario conectado: {isAunthenticated ? 'Sim': 'Não'}</Text>
 
+<Button 
+title='Entrar'
+onPress={handleAuthentication}
+/>
     </View>
   );
 }
